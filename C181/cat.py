ec46caaf122516d4abb4af83d1cc9aa195cc0362
Discandr <78156611+Discandr@users.noreply.github.com>
2021-02-06 09:22:32 +0200

    Add files via upload

---
+++ C181/cat.py
@@ -0,0 +1,12 @@
+from house_of_pets import Cat
+
+cat_1 = Cat("Baron", "boy", 2)
+cat_2 = Cat("Mila", "girl", 1)
+cat_3 = Cat("Nike", "boy", 3)
+cat_4 = Cat("Freeze", "boy", 2)
+
+cat_1.getInfo()
+cat_2.getInfo()
+cat_3.getInfo()
+cat_4.getInfo()
+
