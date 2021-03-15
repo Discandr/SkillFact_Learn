f0a331a65a588d64f94f9274c319fb6ba5226300
Discandr <78156611+Discandr@users.noreply.github.com>
2021-03-15 19:44:05 +0200

    Add files via upload

---
+++ C56/config.py
@@ -0,0 +1,8 @@
+TOKEN = '1641441088:AAFr79T6ayj6e1NlqzprN_wxkRe3svbIzFg'
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+
+}
---
+++ C56/extensions.py
@@ -0,0 +1,40 @@
+import requests
+import json
+from config import keys
+
+class ConvertionException(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def converter(quote: str,  base: str, amount: str):
+
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}.')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}.')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}.')
+
+        #https: // api.exchangeratesapi.io / latest?base = {quote_ticker}&symbols = {base_ticker}
+       # r = requests.get(f"https://api.exchangeratesapi.io/latest?base={base_key}&symbols={sym_key}")
+        r = requests.get(f'https://api.exchangeratesapi.io/latest?base={base_ticker}&symbols={quote_ticker}')
+
+        resp = json.loads(r.content)
+        new_price = resp['rates'][quote_ticker] * amount
+        new_price = round(new_price, 3)
+        total_price =  f"Цена {amount} {base} в {quote} : {new_price}"
+
+
+        return total_price
